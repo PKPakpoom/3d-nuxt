c40baf43b4e16ae0bb98ec3e7ffcd4c7f5088b30
PKPakpoom <pai38985@gmail.com>
2024-08-02 01:12:02 +0700

    fsde

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,31 @@
 <template>
+    {{ x }} {{ y }}
+    <TresCanvas clear-color="#008080" window-size class="canva">
+        <TresPerspectiveCamera
+            ref="cameraRef"
+        />
+        <TresMesh
+            ref="boxRef"
+            :position="[0, 5, 0]"
+        >
+            <TresBoxGeometry :args="[1, 1, 1]" />
+            <TresMeshNormalMaterial />
+        </TresMesh>
+        <Text3D
+            :position="[0, 0, 0]"
+            font="https://raw.githubusercontent.com/Tresjs/assets/main/fonts/FiraCodeRegular.json"
+        >
+            Hello ewew ewewe
+        </Text3D>
+        <TresDirectionalLight :position="[0, 2, 4]" :intensity="1.2" cast-shadow />
+        <TresMeshMatcapMaterial />
+    </TresCanvas>
     <div ref="el" class="container">
-        <TresCanvas clear-color="#008080" window-size class="canva">
-            <TresPerspectiveCamera
-                ref="cameraRef"
-            />
-            <!-- <OrbitControls 
-            /> -->
-            <TresMesh
-                ref="boxRef"
-                :position="[2, 2, 0]"
-            >
-                <TresBoxGeometry :args="[1, 1, 1]" />
-                <TresMeshNormalMaterial />
-            </TresMesh>
-            <!-- <TresGridHelper /> -->
-        </TresCanvas>
         <section class="section">
             <h1>My Portfolio</h1>
         </section>
         <section class="section">
             <h2>My projects</h2>
-            {{ x }} {{ y }} {{ isScrolling }} {{ arrivedState }} {{ directions }}
         </section>
         <section class="section">
             <h2>Contact me</h2>
@@ -38,18 +43,25 @@
     }
 
     .container {
-        color: white;
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100vw;
         height: 100vh;
-        width: 100%;
-        overflow: scroll;
-        overflow-x: hidden;
-        scrollbar-width: none;
-        scroll-snap-type: y mandatory;
-        position: relative;
+        overflow-y: scroll;
+        scroll-snap-type: y proximity;
     }
-
+    .container::-webkit-scrollbar {
+        width: 0px;
+        background: transparent;
+    }
+    
+    .container::-webkit-scrollbar-thumb {
+        background: transparent;
+    }
     .section {
         display: flex;
+        scroll-snap-align: center;
         align-items: center;
         height: 100vh;
         position: relative;
@@ -60,17 +72,13 @@
         padding-left: 10%;
         padding-right: 10%;
         will-change: transform;
-        overflow-y: hidden;
     }
 
-    section:nth-child(odd) {
-        justify-content: flex-end;
-    }
 </style>
 
 <script setup lang="ts">
 import { TresCanvas } from '@tresjs/core'
-import { OrbitControls } from '@tresjs/cientos'
+import { Text3D } from '@tresjs/cientos'
 import { shallowRef } from 'vue'
 import { useScroll } from '@vueuse/core'
 
@@ -80,8 +88,6 @@ const cameraRef = shallowRef()
 
 const el = ref(null)
 const { x, y, isScrolling, arrivedState, directions } = useScroll(el)
-
-let scroll = 0
 
 let radius = 5
 let angle = 0
